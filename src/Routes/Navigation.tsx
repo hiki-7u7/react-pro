63f632375b7c0f519a35bc1b40c8ff18e53ce254
Nini <raychu701@gmail.com>
2023-02-27 06:18:56 -0500

    main listo

--- src/Routes/Navigation.tsx
+++ src/Routes/Navigation.tsx
@@ -1,9 +1,4 @@
 import { BrowserRouter, Routes, Route, NavLink, Navigate } from 'react-router-dom';
-import { ShopingPage } from '../02-components-patterns/pages/ShopingPage';
-
-// import logo from '../assets/react.svg';
-
-// import { routes } from './routes';
 
 
 export const Navigation = () => {
@@ -24,30 +19,18 @@ export const Navigation = () => {
                 <li>
                   <NavLink to="/users" className={ ({ isActive }) => isActive ?'nav-active' :''} >Users</NavLink>
                 </li>
-              {/* { 
-                routes.map( li => ( 
-                  <li key={li.name}>
-                    <NavLink to={li.to} className={ ({ isActive }) => isActive ?'nav-active' :''} >{li.name}</NavLink>
-                  </li>
-                ))
-              } */}
 
             </ul>
           </nav>
 
           <Routes>
 
-              <Route path='/shopingPage' element={ <ShopingPage /> } />
+              <Route path='/shopingPage' element={ <h1>ShoppinPage</h1> } />
               <Route path='/about' element={ <h1>About</h1> } />
               <Route path='/users' element={ <h1>Users</h1> } />
 
               <Route path='*' element={ <Navigate to='/shopingPage' /> } />
-              {/* {
-                routes.map( route => (
-                  <Route key={route.name} path={route.path} element= { <route.Component /> } />
-                ))
-              } */}
-
+              
           </Routes>
         </div>
       </BrowserRouter>
